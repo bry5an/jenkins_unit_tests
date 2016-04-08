04e95f10f77a2d62ded2e20e7f89cad4ce290f1e
Bryan Banks <bry5an@gmail.com>
2016-04-08 15:10:21 -0400

    added simple calculator with unit tests

---
+++ UnitTestProject1/CalcTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication4;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void TestAddFunctionality()
+        {
+            Calculatron Calc = new Calculatron();
+            Assert.AreEqual(Calc.Add(1, 3),(1 + 3));
+        }
+
+        [TestMethod]
+        public void TestSubtractFunctionality()
+        {
+            Calculatron Calc = new Calculatron();
+            Assert.AreEqual(Calc.Subtract(1, 3), (1 - 3));
+        }
+
+        [TestMethod]
+        public void TestMultiplyFunctionality()
+        {
+            Calculatron Calc = new Calculatron();
+            Assert.AreEqual(Calc.Multiply(1, 3), (1 * 3));
+        }
+
+        [TestMethod]
+        public void TestDivisionFunctionality()
+        {
+            Calculatron Calc = new Calculatron();
+            Assert.AreEqual(Calc.Divide(6, 3), (6 / 3));
+        }
+    }
+}
